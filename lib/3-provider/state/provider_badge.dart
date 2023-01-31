b37df002c1fe28b9500270ab2f8e56cade3ff66a
nero <devstory.co.kr@gmail.com>
2023-01-31 16:51:42 +0900

    3-provider : Add ProviderBadge.

---
+++ lib/3-provider/state/provider_badge.dart
@@ -0,0 +1,24 @@
+import 'package:apple_store/3-provider/state/provider_cart.dart';
+import 'package:flutter/material.dart';
+
+class ProviderBadge with ChangeNotifier {
+  ProviderBadge({
+    required this.providerCart,
+  }) {
+    providerCart.addListener(providerCartListener);
+  }
+
+  int counter = 0;
+  final ProviderCart providerCart;
+
+  void providerCartListener() {
+    counter = providerCart.cartProductList.length;
+    notifyListeners();
+  }
+
+  @override
+  void dispose() {
+    providerCart.removeListener(providerCartListener);
+    super.dispose();
+  }
+}
