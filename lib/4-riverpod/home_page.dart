b240ccfac179d28858613dadb41986ef30643b94
nero <devstory.co.kr@gmail.com>
2023-02-02 17:18:45 +0900

    4-riverpod : Add RiverpodCart.

--- lib/4-riverpod/home_page.dart
+++ lib/4-riverpod/home_page.dart
@@ -2,6 +2,7 @@ import 'package:apple_store/4-riverpod/cart.dart';
 import 'package:apple_store/4-riverpod/store.dart';
 import 'package:apple_store/common/bottom_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -16,23 +17,25 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: IndexedStack(
-        index: currentIndex,
-        children: const [
-          /// Store
-          Store(),
+    return ProviderScope(
+      child: Scaffold(
+        body: IndexedStack(
+          index: currentIndex,
+          children: const [
+            /// Store
+            Store(),
 
-          /// Cart
-          Cart(),
-        ],
-      ),
-      bottomNavigationBar: BottomBar(
-        currentIndex: currentIndex,
-        cartTotal: "0",
-        onTap: (index) => setState(() {
-          currentIndex = index;
-        }),
+            /// Cart
+            Cart(),
+          ],
+        ),
+        bottomNavigationBar: BottomBar(
+          currentIndex: currentIndex,
+          cartTotal: "0",
+          onTap: (index) => setState(() {
+            currentIndex = index;
+          }),
+        ),
       ),
     );
   }
---
+++ lib/4-riverpod/state/riverpod_cart.dart
@@ -0,0 +1,22 @@
+import 'package:apple_store/common/product.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final cartProvider = NotifierProvider<RiverpodCart, List<Product>>(() {
+  return RiverpodCart();
+});
+
+class RiverpodCart extends Notifier<List<Product>> {
+  @override
+  List<Product> build() {
+    return [];
+  }
+
+  /// 상품 클릭
+  void onProductPressed(Product product) {
+    if (state.contains(product)) {
+      state.remove(product);
+    } else {
+      state.add(product);
+    }
+  }
+}
