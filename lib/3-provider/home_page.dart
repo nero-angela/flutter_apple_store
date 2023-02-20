56742523273d257187b9baeab2b581a3a0ea10c3
nero <devstory.co.kr@gmail.com>
2023-02-20 18:51:30 +0900

    3-provider : Add ProviderCart.

--- lib/3-provider/home_page.dart
+++ lib/3-provider/home_page.dart
@@ -1,7 +1,9 @@
 import 'package:apple_store/3-provider/cart.dart';
+import 'package:apple_store/3-provider/state/provider_cart.dart';
 import 'package:apple_store/3-provider/store.dart';
 import 'package:apple_store/common/bottom_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -16,23 +18,30 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: IndexedStack(
-        index: currentIndex,
-        children: const [
-          /// Store
-          Store(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => ProviderCart(),
+        )
+      ],
+      child: Scaffold(
+        body: IndexedStack(
+          index: currentIndex,
+          children: const [
+            /// Store
+            Store(),
 
-          /// Cart
-          Cart(),
-        ],
-      ),
-      bottomNavigationBar: BottomBar(
-        currentIndex: currentIndex,
-        cartTotal: "0",
-        onTap: (index) => setState(() {
-          currentIndex = index;
-        }),
+            /// Cart
+            Cart(),
+          ],
+        ),
+        bottomNavigationBar: BottomBar(
+          currentIndex: currentIndex,
+          cartTotal: "0",
+          onTap: (index) => setState(() {
+            currentIndex = index;
+          }),
+        ),
       ),
     );
   }
---
+++ lib/3-provider/state/provider_cart.dart
@@ -0,0 +1,17 @@
+import 'package:apple_store/common/product.dart';
+import 'package:flutter/material.dart';
+
+class ProviderCart with ChangeNotifier {
+  /// 카트에 담긴 상품 목록
+  List<Product> cartProductList = [];
+
+  /// 상품 클릭
+  void onProductPressed(Product product) {
+    if (cartProductList.contains(product)) {
+      cartProductList.remove(product);
+    } else {
+      cartProductList.add(product);
+    }
+    notifyListeners();
+  }
+}
